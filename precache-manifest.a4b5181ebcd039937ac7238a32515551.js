e3b975303ba1487befbdfbb71cbe9b7b07013140
lucas93298 <lucas.fernandez93@hotmail.com>
2020-07-13 13:43:00 -0300

    Updates

---
+++ precache-manifest.a4b5181ebcd039937ac7238a32515551.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073110ef88174b13b27781981940fb16",
+    "url": "/my-app/Fronted/index.html"
+  },
+  {
+    "revision": "1bb33f5985ddd0b788dd",
+    "url": "/my-app/Fronted/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "931717b71d5f96666396",
+    "url": "/my-app/Fronted/static/css/main.96957ba9.chunk.css"
+  },
+  {
+    "revision": "1bb33f5985ddd0b788dd",
+    "url": "/my-app/Fronted/static/js/2.ea79f3db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/Fronted/static/js/2.ea79f3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931717b71d5f96666396",
+    "url": "/my-app/Fronted/static/js/main.6da5b239.chunk.js"
+  },
+  {
+    "revision": "448f80560927b934e49d",
+    "url": "/my-app/Fronted/static/js/runtime-main.2b360898.js"
+  },
+  {
+    "revision": "e36bb1c008738b6dece21c4b52a0d4ea",
+    "url": "/my-app/Fronted/static/media/chip.e36bb1c0.png"
+  },
+  {
+    "revision": "001ecdbde5bd1f898c6efcfd4a5ea6ef",
+    "url": "/my-app/Fronted/static/media/hojas.001ecdbd.jpg"
+  }
+]);
